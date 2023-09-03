1e2c8e59b4e51d3bb4d4c2c767c3f1231ae49c3b
Emilio Pino <pinoen@yahoo.com.ar>
2023-09-03 18:12:21 -0300

    resolve exercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,21 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// My first attemp solution:
+/* function reverseInt(n) {
+  // This is needed to get a positive or negative result according to the input (the same in one line in the return)
+  // let aux;
+  // if (n >= 0) {
+  //   aux = 1
+  // } else {
+  //   aux = -1
+  // }
+  return parseInt(n.toString().split('').reverse().join('')) * (n >= 0 ? 1 : -1)
+} */
+
+// First attemp using Math.sign()
+function reverseInt(n) {
+  return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n)
+}
 
 module.exports = reverseInt;
